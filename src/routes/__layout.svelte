6056ab68d8a97bc00ded928078002d574429dca7
szuturon <ronnyszutu@gmail.com>
2022-04-30 23:23:05 -0400

    Pages, Routing and Layouts in Svelte Kit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<header>
+    <h1>Welcome</h1>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About Us</a>
+    </nav>
+</header>
+
+<main>    
+    <slot />
+</main>
+
+<footer>Copyright 2022</footer>
---
+++ src/routes/about.svelte
@@ -0,0 +1,4 @@
+<h1>About us</h1>
+<p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate dolorem sunt quod fugiat quis autem nobis dolore blanditiis fugit, inventore molestiae non odit a quas ipsam alias omnis doloribus iure!
+</p>
