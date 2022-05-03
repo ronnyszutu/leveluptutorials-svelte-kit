564f35cc3915dfec85f2e266e3707bbdf193f460
szuturon <ronnyszutu@gmail.com>
2022-05-03 00:19:35 -0400

    Dynamic Imports and Frontmatter

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,24 +1,28 @@
 <script context="module">
     export async function load({ params }) {
-    const post = {
-      // slug comes directly from the file title
-      title: params.slug,
-      date: new Date(),
-      body: "lorem ipsum",
-    };
-    return {
-      props: {
-        post
-      }
+        const Hello = (await import(`../../posts/${params.slug}.md`)).default;
+        const post = {
+            // slug comes directly from the file title
+            // title: params.slug,
+            // date: new Date(),
+            // body: "lorem ipsum",
+        };
+        return {
+            props: {
+                Hello,
+                // post
+            }
+        }
     }
-  }
 </script>
 
 <script>
-    import Hello from '../../posts/hello.md';
-    export let post;
+    // import Hello from '../../posts/hello.md';
+    // export let post;
+    export let Hello;
 </script>
 
-<Hello />
+<!-- <Hello /> -->
+<svelte:component this={Hello}/> <!-- copied from cmd -->
 
-<h3>{post.title}</h3>
+<!-- <h3>{post.title}</h3> -->
