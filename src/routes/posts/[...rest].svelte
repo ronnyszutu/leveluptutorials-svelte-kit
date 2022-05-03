ef97d00e7150cfc5afce5bfa320cb74e07c8fe59
szuturon <ronnyszutu@gmail.com>
2022-05-03 00:02:27 -0400

    Loading Data Before Initial Render

--- src/routes/posts/[...rest].svelte
+++
@@ -1,1 +0,0 @@
-<h2>Rest Route</h2>
---
+++ src/routes/posts/[slug].svelte
@@ -0,0 +1,21 @@
+<script context="module">
+    export async function load({ page }) {
+    const post = {
+      // slug comes directly from the file title
+      title: page.params.slug,
+      date: new Date(),
+      body: "lorem ipsum",
+    };
+    return {
+      props: {
+        post
+      }
+    }
+  }
+</script>
+
+<script>
+    export let post;
+</script>
+
+<h3>{post.title}</h3>
